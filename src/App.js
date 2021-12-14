967e69cafacb32c6cce6a8704ec4f83828da44ea
Praveen Kumar <praveenkumar@fleetcor.com>
2021-12-14 00:42:17 -0500

    Initial Check in

--- src/App.js
+++ src/App.js
@@ -7,16 +7,17 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {/* Edit <code>src/App.js</code> and save to reload. */}
+          This is Openshift deployment test for MDUI project
         </p>
-        <a
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
